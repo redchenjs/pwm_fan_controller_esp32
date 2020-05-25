1a53935563962373ea046e417074db3d15ef23ea
Jack Chen <redchenjs@live.com>
2020-05-25 14:22:56 +0800

    qc: code clean up

--- main/src/user/qc.c
+++ main/src/user/qc.c
@@ -27,30 +27,30 @@ void qc_init(qc_idx_t idx)
     dac_output_disable(DAC_CHANNEL_1);
     dac_output_disable(DAC_CHANNEL_2);
 
-    vTaskDelay(50 / portTICK_RATE_MS);
-
     adc2_config_channel_atten(ADC_CHANNEL_9, ADC_ATTEN_DB_0);
 
     dac_output_enable(DAC_CHANNEL_1);
     dac_output_voltage(DAC_CHANNEL_1, 255 * (0.325 / 3.3));
 
+    vTaskDelay(50 / portTICK_RATE_MS);
+
     adc2_get_raw(ADC_CHANNEL_9, ADC_WIDTH_BIT_12, &dp_raw);
 
-    if (dp_raw < 100) {
+    if (dp_raw < 255) {
         ESP_LOGI(TAG, "SDP 5V");
         dac_output_disable(DAC_CHANNEL_1);
         return;
     }
 
-    vTaskDelay(1250 / portTICK_RATE_MS);
+    vTaskDelay(1000 / portTICK_RATE_MS);
 
     do {
         adc2_get_raw(ADC_CHANNEL_9, ADC_WIDTH_BIT_12, &dp_raw);
 
         vTaskDelay(10 / portTICK_RATE_MS);
-    } while (dp_raw > 100 && ++dp_cnt < 100);
+    } while (dp_raw > 255 && ++dp_cnt < 100);
 
-    if (dp_raw > 100) {
+    if (dp_raw > 255) {
         ESP_LOGI(TAG, "DCP 5V");
         return;
     }
