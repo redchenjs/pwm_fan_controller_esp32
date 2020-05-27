fe703ec9fcdcd8c17f50db4951553b01888bf08d
Jack Chen <redchenjs@live.com>
2020-05-27 15:49:19 +0800

    pwr: code refactoring

--- main/src/user/pwr.c
+++ main/src/user/pwr.c
@@ -42,17 +42,15 @@ void pwr_init(pwr_idx_t idx)
 {
     int dp_raw = 0, dp_cnt = 0;
 
-    dac_output_voltage(DAC_CHANNEL_1, 0);
-    dac_output_voltage(DAC_CHANNEL_2, 0);
+    adc2_config_channel_atten(ADC_CHANNEL_9, ADC_ATTEN_DB_0);
+
     dac_output_disable(DAC_CHANNEL_1);
     dac_output_disable(DAC_CHANNEL_2);
 
-    adc2_config_channel_atten(ADC_CHANNEL_9, ADC_ATTEN_DB_0);
-
-    dac_output_enable(DAC_CHANNEL_1);
-    dac_output_voltage(DAC_CHANNEL_1, 255 * (0.325 / 3.3));
-
     vTaskDelay(50 / portTICK_RATE_MS);
+
+    dac_output_voltage(DAC_CHANNEL_1, 255 * (0.325 / 3.3));
+    dac_output_enable(DAC_CHANNEL_1);
 
     adc2_get_raw(ADC_CHANNEL_9, ADC_WIDTH_BIT_12, &dp_raw);
 
@@ -64,13 +62,13 @@ void pwr_init(pwr_idx_t idx)
         goto pwr_exit;
     }
 
-    vTaskDelay(1000 / portTICK_RATE_MS);
-
     do {
         adc2_get_raw(ADC_CHANNEL_9, ADC_WIDTH_BIT_12, &dp_raw);
 
         vTaskDelay(10 / portTICK_RATE_MS);
-    } while (dp_raw > 255 && ++dp_cnt < 100);
+    } while (dp_raw > 255 && ++dp_cnt < 150);
+
+    dac_output_disable(DAC_CHANNEL_1);
 
     if (dp_raw > 255) {
         pwr_mode = PWR_IDX_DCP;
@@ -79,30 +77,30 @@ void pwr_init(pwr_idx_t idx)
     }
 
     switch (idx) {
+        default:
         case PWR_IDX_QC_5V:
             pwr_mode = PWR_IDX_QC_5V;
 
-            dac_output_enable(DAC_CHANNEL_2);
             dac_output_voltage(DAC_CHANNEL_1, 255 * (0.6 / 3.3));
             dac_output_voltage(DAC_CHANNEL_2, 255 * (0.0 / 3.3));
+            dac_output_enable(DAC_CHANNEL_1);
+            dac_output_enable(DAC_CHANNEL_2);
             break;
         case PWR_IDX_QC_9V:
             pwr_mode = PWR_IDX_QC_9V;
 
-            dac_output_enable(DAC_CHANNEL_2);
             dac_output_voltage(DAC_CHANNEL_1, 255 * (3.3 / 3.3));
             dac_output_voltage(DAC_CHANNEL_2, 255 * (0.6 / 3.3));
+            dac_output_enable(DAC_CHANNEL_1);
+            dac_output_enable(DAC_CHANNEL_2);
             break;
         case PWR_IDX_QC_12V:
             pwr_mode = PWR_IDX_QC_12V;
 
-            dac_output_enable(DAC_CHANNEL_2);
             dac_output_voltage(DAC_CHANNEL_1, 255 * (0.6 / 3.3));
             dac_output_voltage(DAC_CHANNEL_2, 255 * (0.6 / 3.3));
-            break;
-        default:
-            pwr_mode = PWR_IDX_DCP;
-
+            dac_output_enable(DAC_CHANNEL_1);
+            dac_output_enable(DAC_CHANNEL_2);
             break;
     }
 
