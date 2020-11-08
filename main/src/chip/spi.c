3300ef92ce96cdee2828b53afee7ef5d95d9ac55
Jack Chen <redchenjs@live.com>
2020-11-08 22:26:40 +0800

    chip: code refactoring

--- main/src/chip/spi.c
+++ main/src/chip/spi.c
@@ -24,16 +24,16 @@ void hspi_init(void)
         .sclk_io_num = CONFIG_SPI_SCLK_PIN,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
-        .max_transfer_sz = ST7789_SCREEN_WIDTH*ST7789_SCREEN_HEIGHT*2
+        .max_transfer_sz = ST7789_SCREEN_WIDTH * ST7789_SCREEN_HEIGHT * 2
     };
     ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, 1));
 
     spi_device_interface_config_t devcfg = {
         .mode = 0,                                // SPI mode 0
         .spics_io_num = CONFIG_SPI_CS_PIN,        // CS pin
-        .clock_speed_hz = 40000000,               // Clock out at 40 MHz
-        .queue_size = 2,                          // We want to be able to queue 6 transactions at a time
-        .pre_cb = st7789_setpin_dc,               // Specify pre-transfer callback to handle D/C line
+        .clock_speed_hz = 40000000,               // clock out at 40 MHz
+        .pre_cb = st7789_setpin_dc,               // specify pre-transfer callback to handle D/C line
+        .queue_size = 2,                          // we want to be able to queue 2 transactions at a time
         .flags = SPI_DEVICE_3WIRE | SPI_DEVICE_HALFDUPLEX
     };
     ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &hspi));
