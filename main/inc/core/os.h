b14e8b8353ded94a2179b82bf3f2484a65759e0f
Jack Chen <redchenjs@live.com>
2020-11-08 22:15:54 +0800

    core: code refactoring

--- main/inc/core/os.h
+++ main/inc/core/os.h
@@ -12,21 +12,25 @@
 #include "freertos/event_groups.h"
 
 typedef enum user_event_group_bits {
-    OS_PWR_SLEEP_BIT   = BIT0,
-    OS_PWR_RESET_BIT   = BIT1,
+    OS_PWR_DUMMY_BIT = 0x00,
+    OS_PWR_RESET_BIT = BIT0,
+    OS_PWR_SLEEP_BIT = BIT1,
 
-    BLE_GATTS_IDLE_BIT = BIT2,
-    BLE_GATTS_LOCK_BIT = BIT3,
+    GUI_RLD_MODE_BIT = BIT2,
 
-    FAN_RUN_BIT        = BIT4,
-    KEY_RUN_BIT        = BIT5,
-    GUI_RELOAD_BIT     = BIT6,
+    KEY_SCAN_RUN_BIT = BIT3,
+    KEY_SCAN_CLR_BIT = BIT4,
+
+    FAN_CTRL_RUN_BIT = BIT5,
+
+    BLE_GATTS_IDLE_BIT = BIT6,
+    BLE_GATTS_LOCK_BIT = BIT7
 } user_event_group_bits_t;
 
 extern EventGroupHandle_t user_event_group;
 
-extern void os_pwr_sleep_wait(EventBits_t bits);
 extern void os_pwr_reset_wait(EventBits_t bits);
+extern void os_pwr_sleep_wait(EventBits_t bits);
 
 extern void os_init(void);
 
