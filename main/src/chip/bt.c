0281c8d7a87112fd7e028a9ca2d78721736cc0dd
Jack Chen <redchenjs@live.com>
2021-02-11 19:06:31 +0800

    bt: code refactoring

--- main/src/chip/bt.c
+++ main/src/chip/bt.c
@@ -5,6 +5,8 @@
  *      Author: Jack Chen <redchenjs@live.com>
  */
 
+#include <string.h>
+
 #include "esp_bt.h"
 #include "esp_log.h"
 #include "esp_bt_main.h"
@@ -12,20 +14,50 @@
 
 #define TAG "bt"
 
-const char *bt_dev_address = NULL;
+static char bt_mac_string[18] = {0};
+static char ble_mac_string[18] = {0};
+
+static uint8_t bt_mac_address[6] = {0};
+static uint8_t ble_mac_address[6] = {0};
+
+char *bt_get_mac_string(void)
+{
+    return bt_mac_string;
+}
+
+char *ble_get_mac_string(void)
+{
+    return ble_mac_string;
+}
+
+uint8_t *bt_get_mac_address(void)
+{
+    return bt_mac_address;
+}
+
+uint8_t *ble_get_mac_address(void)
+{
+    return ble_mac_address;
+}
 
 void bt_init(void)
 {
 #ifdef CONFIG_ENABLE_BLE_CONTROL_IF
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
 
-    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
-    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
+    esp_bt_controller_config_t conf = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_bt_controller_init(&conf));
+    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BTDM));
     ESP_ERROR_CHECK(esp_bluedroid_init());
     ESP_ERROR_CHECK(esp_bluedroid_enable());
 
-    bt_dev_address = (const char *)esp_bt_dev_get_address();
+    memcpy(bt_mac_address, esp_bt_dev_get_address(), sizeof(bt_mac_address));
+    memcpy(ble_mac_address, esp_bt_dev_get_address(), sizeof(ble_mac_address));
+
+    ble_mac_address[0] |= 0xC0;
+
+    snprintf(bt_mac_string, sizeof(bt_mac_string), MACSTR, MAC2STR(bt_mac_address));
+    snprintf(ble_mac_string, sizeof(ble_mac_string), MACSTR, MAC2STR(ble_mac_address));
 
     ESP_LOGI(TAG, "initialized, bt: 0, ble: 1");
 #else
