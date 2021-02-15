0c0adb4a1e9fb577ce3e4f423ca997edc382e70b
Jack Chen <redchenjs@live.com>
2021-02-15 16:32:15 +0800

    bt_gatts: code cleanup

--- main/src/user/ble_gatts.c
+++ main/src/user/ble_gatts.c
@@ -159,8 +159,7 @@ static void profile_ota_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t 
 
         ota_end();
 
-        EventBits_t uxBits = xEventGroupGetBits(user_event_group);
-        if (!(uxBits & OS_PWR_RESET_BIT) && !(uxBits & OS_PWR_SLEEP_BIT)) {
+        if (!(xEventGroupGetBits(user_event_group) & (OS_PWR_RESET_BIT | OS_PWR_SLEEP_BIT))) {
             ble_gap_start_advertising();
         }
 
